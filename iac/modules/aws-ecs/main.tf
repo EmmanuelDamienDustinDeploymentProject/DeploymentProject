ed081cc0e3ee35b430ed17a5b5eb6b51112fb971
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2025-11-14 11:20:39 -0600

    setup private hosted zone for the mcp server and output the nameservers so I can add it to my account (#25)
    Co-authored-by: Dustin Alandzes <dustin.alandzes@hp.com>

--- iac/modules/aws-ecs/main.tf
+++ iac/modules/aws-ecs/main.tf
@@ -179,21 +179,21 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_security_group" "load_balancer_sg" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound traffic and all outbound traffic"
+  name        = "allow_https"
+  description = "Allow HTTPS inbound traffic and all outbound traffic"
   vpc_id      = data.aws_vpc.default.id
 
   tags = {
-    Name = "allow_http"
+    Name = "allow_https"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
   security_group_id = aws_security_group.load_balancer_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
+  from_port         = 443
   ip_protocol       = "tcp"
-  to_port           = 80
+  to_port           = 443
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_egress_all_traffic_ipv4" {
@@ -220,8 +220,10 @@ resource "aws_lb" "main" {
 
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = data.aws_acm_certificate.main.arn
 
   default_action {
     type             = "forward"
@@ -316,3 +318,19 @@ resource "github_actions_variable" "ecs_cluster_name" {
   variable_name = "ECS_CLUSTER_NAME"
   value         = aws_ecs_cluster.main.name
 }
+
+data "aws_acm_certificate" "main" {
+  domain = var.acm_certificate_domain
+}
+
+data "aws_route53_zone" "main" {
+  name = var.domain
+}
+
+# resource "aws_route53_record" "mcp_subdomain" {
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = format("mcp.%s", var.domain)
+#   type    = "CNAME"
+#   ttl     = 60
+#   records = [aws_lb.main.dns_name]
+# }
