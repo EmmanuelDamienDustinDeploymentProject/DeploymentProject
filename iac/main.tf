ea979fce5fd260e7cdabf5c3d7cba82baeaf98dc
Dustin Alandzes <dustin.alandzes@hp.com>
2025-11-14 15:51:08 -0600

    commit the rest of the files

--- iac/main.tf
+++ iac/main.tf
@@ -41,22 +41,22 @@ resource "aws_acm_certificate" "mcp" {
 
 resource "aws_route53_record" "validate_certificate" {
   allow_overwrite = true
-  name =  tolist(aws_acm_certificate.mcp.domain_validation_options)[0].resource_record_name
-  records = [tolist(aws_acm_certificate.mcp.domain_validation_options)[0].resource_record_value]
-  type = tolist(aws_acm_certificate.mcp.domain_validation_options)[0].resource_record_type
-  zone_id = data.aws_route53_zone.public_zone.zone_id
-  ttl = 60
+  name            = tolist(aws_acm_certificate.mcp.domain_validation_options)[0].resource_record_name
+  records         = [tolist(aws_acm_certificate.mcp.domain_validation_options)[0].resource_record_value]
+  type            = tolist(aws_acm_certificate.mcp.domain_validation_options)[0].resource_record_type
+  zone_id         = data.aws_route53_zone.public_zone.zone_id
+  ttl             = 60
 }
 
 resource "aws_acm_certificate_validation" "validate_certificate" {
-  certificate_arn = aws_acm_certificate.mcp.arn
+  certificate_arn         = aws_acm_certificate.mcp.arn
   validation_record_fqdns = [aws_route53_record.validate_certificate.fqdn]
 }
 
 module "ecs_cluster" {
-  source                 = "./modules/aws-ecs"
-  github_repo_name       = data.github_repository.main.full_name
+  source                         = "./modules/aws-ecs"
+  github_repo_name               = data.github_repository.main.full_name
   ecs_task_environment_variables = []
-  domain                 = data.aws_route53_zone.public_zone.name
-  acm_certificate_domain = aws_acm_certificate.mcp.domain_name
+  domain                         = data.aws_route53_zone.public_zone.name
+  acm_certificate_domain         = aws_acm_certificate.mcp.domain_name
 }
--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,4 +1,4 @@
 output "route53_private_hosted_zone_ns" {
-  value = data.aws_route53_zone.public_zone.name_servers
+  value       = data.aws_route53_zone.public_zone.name_servers
   description = "Values to add to the public hosted zone in dustin's account"
 }
