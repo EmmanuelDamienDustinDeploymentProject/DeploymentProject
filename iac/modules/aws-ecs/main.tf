1bb4e1db0877cfac7721062210594519ff29d3ef
Dustin Alandzes <dustin.alandzes@hp.com>
2025-11-14 11:24:45 -0600

    remove https/443 things for now

--- iac/modules/aws-ecs/main.tf
+++ iac/modules/aws-ecs/main.tf
@@ -191,9 +191,11 @@ resource "aws_security_group" "load_balancer_sg" {
 resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
   security_group_id = aws_security_group.load_balancer_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
+  # from_port         = 443
+  from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = 443
+  # to_port           = 443
+  to_port           = 80
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_egress_all_traffic_ipv4" {
@@ -220,10 +222,12 @@ resource "aws_lb" "main" {
 
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+  # port              = "443"
+  # protocol          = "HTTPS"
+  # ssl_policy        = "ELBSecurityPolicy-2016-08"
+  # certificate_arn   = data.aws_acm_certificate.main.arn
 
   default_action {
     type             = "forward"
@@ -319,13 +323,13 @@ resource "github_actions_variable" "ecs_cluster_name" {
   value         = aws_ecs_cluster.main.name
 }
 
-data "aws_acm_certificate" "main" {
-  domain = var.acm_certificate_domain
-}
-
-data "aws_route53_zone" "main" {
-  name = var.domain
-}
+# data "aws_acm_certificate" "main" {
+#   domain = var.acm_certificate_domain
+# }
+#
+# data "aws_route53_zone" "main" {
+#   name = var.domain
+# }
 
 # resource "aws_route53_record" "mcp_subdomain" {
 #   zone_id = data.aws_route53_zone.main.zone_id
